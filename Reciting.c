6c6ec00aefd9bfd68bc153a6b339e18265615190
22200667 <130739899+22200667@users.noreply.github.com>
2023-05-09 17:08:11 +0900

    Update Reciting.c

--- Reciting.c
+++ Reciting.c
@@ -1,5 +1,6 @@
 /*
-The 
+"But his delight is in the law of the LORD; and in his law doth he meditate day and night." Psalm 1:2
 
 The purpose of the program is to help Christians to recite the Scripture well.
+
 */
